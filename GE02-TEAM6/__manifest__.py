48a8a12398e5bace18a2060caebee36412d4f6e0
cogs-odoo <cogs@odoo.com>
2023-07-24 17:34:01 -0600

    created new module folder

---
+++ GE02-TEAM6/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'GE02 TEAM6',
+    'description': '''
+                    Motorcycle Registry
+                    ====================
+                    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+                    ''',
+    'autor' : 'cogs-odoo',
+    'website' : 'https://github.com/cogs-odoo/team6-kawiil-motors',
+    'category' : 'Kawiil/custom',
+    'application' : 'True',
+    'depends' : ['base','stock','website'],
+    'data' : [],
+    'demo' : [
+        'demo/demo_data.xml',
+    ],
+}
