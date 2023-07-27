cb3f1de6547cdbb0796558965e8c98954fffc1fb
lare-odoo <lare@odoo.com>
2023-07-27 11:12:50 -0600

    [IMP] VIN is now related to the field in "stock_lot"
    This allows us to fill the required fields to create a motorcycle registry using ORM

--- GE07-TEAM6/models/motorcycle_registry.py
+++ GE07-TEAM6/models/motorcycle_registry.py
@@ -4,3 +4,5 @@ class MotorcycleRegistry(models.Model):
     _inherit = 'motorcycle.registry'
 
     lot_id = fields.One2many(comodel_name="stock.lot", string="Lot", inverse_name="motorcycle_id")
+
+    vin = fields.Char(related = "lot_id.name")
