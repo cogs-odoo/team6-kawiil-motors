37891f006233a468da6f7d072f3657988c6e0e98
Jorge Luis Mendez <jlma@MacBook-Air-de-Jorge.local>
2023-07-28 16:19:34 -0600

    [IMP] GE09-TEAM6 : Add  motorcycle specific info to the repair view and create a smart button that makes a relation between the montorcyclke registry to the repair order
    We added a vin field to the repair view to compute specific values related to it, after that we make a relationship between motorcycle registry and repair order, compute a variable
    that counts how many repair orders there are then we created a function to show the tree view of the repair orders with a domain to only show the repair orders that we expect
    
    [IMP] GE09-TEAM6: 'lot_id' is now automatically obtained from motorcycle registry.
    
    This change facilitates the automation of processes like creating a repair through a portal.
    
    [IMP] GE09-TEAM6: Resolved formatting issues
    
    Changed double quotes to single quotes to maintain consistency between files. Also, removed trailing whitespace from line breaks at the end of some files

---
+++ GE09-TEAM6/models/motorcycle_registry.py
@@ -0,0 +1,17 @@
+from odoo import fields, models, api
+
+class MotorcycleRegistry(models.Model):
+    _inherit = 'motorcycle.registry'
+    count_orders = fields.Float(compute = '_compute_count_orders')
+    
+    repair_order_ids = fields.One2many(comodel_name="repair.order", string="Repair orders", inverse_name="registry_id")
+    def action_report_repair_order(self):
+        self.repair_order_ids
+        print('================= repair_order_ids===============',self.repair_order_ids)
+        action = self.env["ir.actions.actions"]._for_xml_id("repair.action_repair_order_tree")
+        action["domain"] = [('registry_id', '=' ,self.id)]
+        return action
+
+    @api.depends('repair_order_ids')
+    def _compute_count_orders(self):
+        self.count_orders = len(self.repair_order_ids)
