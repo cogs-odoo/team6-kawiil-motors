45c8b6f5a253e7c48032e4cbe3e32fd82d14bf46
lare-odoo <lare@odoo.com>
2023-07-27 18:03:57 -0600

    [ADD] Initial commit for GE10-TEAM6, creation of module
    This module adds a widget to the 'website' module that will count the mileage of all the motorcycles in the registry

---
+++ GE10-TEAM6/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "GE10-TEAM6",
+    "summary": "Total Motorcycle Mileage",
+    "description": """
+Motorcycle Registry
+====================
+K'awiil Motors wants to be able to showcase both its motorcycle's reliability and popularity. They want to display the total miles all of their motorcycles have traveled. Since they already keep track of their motorcycle's mileage on their motorcycle registry, this should be fairly straightforward to implement on the backend. They're not sure where they want it to be shown, so they want it to be a website snippet/widget that they can drag/drop anywhere on the website. Bonus points for making an attractive odometer counter snippet
+    """,    
+    "version": "1.0",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": [],
+    "data": [
+        "views/snippets/snippets.xml",
+        "views/snippets/s_mileage.xml",
+    ],
+    "demo": [],
+    "author": "kauil-motors",
+    "website": "www.odoo.com",
+    "application": False,
+}
